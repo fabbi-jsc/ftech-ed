7900a80d5849ca1584f2a7ea0aa247def818616a
phucjshin <phucnv.94ptit@gmail.com>
2018-04-26 16:14:18 +0700

    phuc edit póition

--- data/phuc-team-info.js
+++ data/phuc-team-info.js
@@ -1,6 +1,7 @@
 
 const data = [
-	{id: 1, position: 'dev', fullname: 'NguyenVanPhuc', phone: '124765432', hobby: 'Code' },
+	{id: 1, position: 'de', fullname: 'NguyenVanPhuc', phone: '124765432', hobby: 'Code' },
 	{id: 2, position: 'test', fullname: 'TranVanKhang', phone: '123454321', hobby: 'Security' },
 
 ]
+	
