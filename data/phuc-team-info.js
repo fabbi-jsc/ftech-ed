0688311df80b0ac2c84cf53e2bc5cd2f7f7de647
Nguyễn Văn Trọng <nguyenvantrongmanutd@gmail.com>
2018-04-27 00:28:58 +0700

    commit by trong

--- data/phuc-team-info.js
+++ data/phuc-team-info.js
@@ -2,6 +2,7 @@
 const data = [
 	{id: 1, position: 'security', fullname: 'NguyenVanPhuc', phone: '124765432', hobby: 'Code' },
 	{id: 2, position: 'test', fullname: 'TranVanKhang', phone: '123454321', hobby: 'Security' },
+	{id: 3, position: 'code', fullname: 'NguyenVanTrong', phone: '6969696', hobby: 'Security' },
 
 ]
 	
