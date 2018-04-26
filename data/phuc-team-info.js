38314b6bc1e537c88c2fab20fb80677eb0321a7d
khangtran <khangtran3007@gmail.com>
2018-04-26 16:06:36 +0700

    edit possion Phuc

--- data/phuc-team-info.js
+++ data/phuc-team-info.js
@@ -1,6 +1,6 @@
 
 const data = [
-	{id: 1, position: 'dev', fullname: 'NguyenVanPhuc', phone: '124765432', hobby: 'Code' },
+	{id: 1, position: 'giamdoc', fullname: 'NguyenVanPhuc', phone: '124765432', hobby: 'Code' },
 	{id: 2, position: 'test', fullname: 'TranVanKhang', phone: '123454321', hobby: 'Security' },
 
 ]
